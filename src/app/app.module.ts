95f5fd4597a5946a26f8ee97ca4f56871b470b7d
YouZhou <zhouyou0528@sina.com>
2018-06-19 20:07:00 -0400

    style fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -16,6 +22,11 @@ import { SignupformComponent } from './signupform/signupform.component';
     SignupformComponent
   ],
   imports: [
+    MatIconModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/signinform/signinform.component.ts
+++ src/app/signinform/signinform.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninformComponent implements OnInit {
 
+  hide = true;
+  
   constructor() { }
 
   ngOnInit() {
